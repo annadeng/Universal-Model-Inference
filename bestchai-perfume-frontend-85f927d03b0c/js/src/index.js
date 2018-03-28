2f92323e9ccc180304e58d713dcfed9af15326d6
adeng <adeng@umass.edu>
2018-03-28 19:35:33 -0400

    local import done

--- bestchai-perfume-frontend-85f927d03b0c/js/src/index.js
+++ bestchai-perfume-frontend-85f927d03b0c/js/src/index.js
@@ -16,7 +16,7 @@ function fetchModel() {
         var parameters =  { logfile: $("#logtext").val(), args: $("#argsfield").val(), requestID: requestID };
         $.ajax({
             type:"POST", 
-            url:"http://localhost:12345/json.php", 
+            url:"http://localhost:8080/json.php", 
             data:parameters
         }).done(function(model) {
             console.log(parameters)
