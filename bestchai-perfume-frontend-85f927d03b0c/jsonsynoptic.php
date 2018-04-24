e9a01f54b7eca3e836c9238cb6a81d2a98360b1d
adeng <adeng@umass.edu>
2018-04-24 15:35:01 -0400

    added string token log

--- bestchai-perfume-frontend-85f927d03b0c/jsonsynoptic.php
+++ bestchai-perfume-frontend-85f927d03b0c/jsonsynoptic.php
@@ -20,7 +20,7 @@ fwrite($jsonfh, "-o ./tmp/json\n");
 fwrite($jsonfh, "-j\n");
 fclose($jsonfh);
 $output = shell_exec(' ./synoptic.sh --noModelOutput -c ./tmp/jsonargs.txt ' . "./tmp/log.txt 2>&1");
-shell_exec("./synoptic.sh --outputPathPrefix ./tmp/test -c ./tmp/jsonargs.txt ./tmp/log.txt 2>&1");
+shell_exec("./synoptic.sh --outputPathPrefix ./tmp/synoptictest -c ./tmp/jsonargs.txt ./tmp/log.txt 2>&1");
 
 $json = file_get_contents('./tmp/json.json');
 
